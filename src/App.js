061ff7dfaec20b351983690dc592ad04acdfe46f
Kamal Bhera <kamalbhera96@gmail.com>
2022-12-24 23:18:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
+import React, { useState } from "react";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [heading, setHeading] = useState();
+  const [fontSize, setFontsize] = useState(12);
+  const hendelChange = (e) => {
+    let result;
+    console.log(e.target.id);
+    switch (e.target.id) {
+      case 'increment':
+        result = fontSize + 2
+        break;
+      case 'decrement':
+        result = fontSize - 2;
+          break;
+      default:
+        break;  
+    }
+    setFontsize(result)
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Font <code>Resizer</code> React Hooks.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <div class="input-group plus-minus-input">
+          <div class="input-group-button">
+            <button type="button" id="decrement" onClick={hendelChange} class="button hollow circle" data-quantity="minus" data-field="quantity">
+            -
+            </button>
+          </div>
+          <input class="input-group-field" onChange={(e) => setHeading(e.target.value)} type="text" name="quantity" />
+          <div class="input-group-button">
+            <button type="button" id="increment" onClick={hendelChange} class="button hollow circle" data-quantity="plus" data-field="quantity">
+              +
+            </button>
+          </div>
+        </div>
+        <div className="output" style={{ fontSize: `${fontSize}px` }}>
+        {heading}
+        </div>
       </header>
     </div>
   );
